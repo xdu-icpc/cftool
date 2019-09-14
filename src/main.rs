9b576c571e788db4da8799192d331d7109dc9ac3
Xi Ruoyao <xry111@mengyan1223.wang>
2019-09-15 01:05:47 +0800

    v0.3.1

--- src/main.rs
+++ src/main.rs
@@ -247,7 +247,7 @@ enum Action {
 fn main() {
     use clap::{App, Arg};
     let matches = App::new("XDU-ICPC cftool")
-        .version("0.3.0")
+        .version("0.3.1")
         .author("Xi Ruoyao <xry111@mengyan1223.wang>")
         .about("A command line tool for submitting code to Codeforces")
         .arg(
@@ -363,7 +363,7 @@ fn main() {
         .init()
         .unwrap();
 
-    info!("{}", "this is XDU-ICPC cftool, version 0.3.0");
+    info!("{}", "this is XDU-ICPC cftool, version 0.3.1");
 
     let mut action = Action::None;
 
